4eceae901eef21d28444c688c390da3d69975cd9
GrahamJReid <grahamreidtechnology@gmail.com>
2022-11-14 14:45:58 -0600

    setup and delivery paired

--- public/main.js
+++ public/main.js
@@ -28,21 +28,20 @@ const getRequest = () => new Promise((resolve, reject) => {
     .then((data) => resolve(data))
     .catch(reject);
 });
-const punchLineOnDom = () => {
-  getRequest().then((response) => {
-    console.warn('is this making it to the dom?');
-    renderToDom('#setup', response.delivery);
-  });
-};
+
 const setupOnDom = () => {
   getRequest().then((response) => {
     console.warn(response);
-    renderToDom('#setup', response.setup);
+    renderToDom('#punchLineDiv', response.setup);
+    const punchpunch = response.delivery;
     document.querySelector('#app').innerHTML = `
    
     <button class="btn btn-danger" id="punchLine">Get Punchline</button>
     
   `;
+    const punchLineOnDom = () => {
+      renderToDom('#setup', punchpunch);
+    };
     document.querySelector('#punchLine').addEventListener('click', punchLineOnDom);
   });
 };
